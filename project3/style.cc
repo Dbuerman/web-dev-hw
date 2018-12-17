0b966c8443a2bd773750b085e330272d2ac564b5
Dakota <dakota.buerman@umconnect.umt.edu>
2018-12-17 13:00:44 -0700

    Incompleteproject3

---
+++ project3/style.cc
@@ -0,0 +1,16 @@
+.p1 {
+   font-family: sans-serif;
+}
+.p2 {
+   font-family: serif;
+}
+.p3 {
+   font-family: monospace;
+}
+
+/* Specific: */
+.p4 {
+   font-family: Times;
+}
+point, font-size: 12px
+pixel, font-size: 16px
